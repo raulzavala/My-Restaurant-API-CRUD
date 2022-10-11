82aebd201c3ac11a72db4b2247107e1b0b67348c
Raul Zavala <iscraulzavala@gmail.com>
2022-10-10 19:09:27 -0700

    My Restaurant Project CRUD

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDoc = require('./swagger.json');
+
+var restaurants = [{id:0,name:"Woodshill"},{id:1,name:"Great Wall"}];
+
+const app = express();
+app.use(express.json());
+
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDoc));
+
+app.get('/restaurants',(req,res)=>{
+    res.send(restaurants);
+})
+
+app.post('/restaurant',(req,res)=>{
+    restaurants.push({id:req.body.id,name:req.body.name});
+    res.send(`${JSON.stringify(req.body.name)} created`);
+
+})
+
+app.listen(3000, ()=>{
+    console.log('Console running on port 3000');
+})
